7f9913caa9ac22ca18ea896db49d44f1b675c0bf
Thomas Amberg <tamberg@yaler.net>
2014-04-24 14:54:07 +0200

    Added read timeout to YalerWiFiServer.

--- Arduino/YalerWiFiServer/YalerWiFiServer.cpp
+++ Arduino/YalerWiFiServer/YalerWiFiServer.cpp
@@ -23,12 +23,12 @@ WiFiClient YalerWiFiServer::available()
   const char *host = _host;
   uint16_t port = _port;
   int x[3];
-  //Serial.println("connecting");
+  //Serial.println("connecting...");
   acceptable = client.connect(host, port);
   if (acceptable) {
     //Serial.println("connected");
     do {
-      //Serial.println("sending request");
+      //Serial.println("sending request...");
       client.print("POST /");
       client.print(_id);
       client.print(" HTTP/1.1\r\n");
@@ -37,18 +37,30 @@ WiFiClient YalerWiFiServer::available()
       client.print("Host: ");
       client.print(host);
       client.print("\r\n\r\n");
-      while (!client.available()) {}
-      //Serial.println("receiving response");
-      for (int j = 0; j != 12; j++) {
-        x[j % 3] = client.read();
-        //Serial.print((char) x[j % 3]);
+      //Serial.println("receiving response...");
+      boolean timeout = false;
+      for (int j = 0; !timeout && (j != 12); j++) {
+        int count = -32500;
+        while (!timeout && !client.available()) {
+          delay(1);
+          if (count == 32500) { // 75000 ms
+            timeout = true;
+            //Serial.print("timeout!");
+          }
+          count++;
+        }
+        if (!timeout) {
+          x[j % 3] = client.read();
+          //Serial.print((char) x[j % 3]);
+        }
       }
       //Serial.println();
-      acceptable = client.find("\r\n\r\n");
+      acceptable = (!timeout && client.find("\r\n\r\n"));
     } while (acceptable && ((x[0] == '2') && (x[1] == '0') && (x[2] == '4')));
     if (!acceptable || (x[0] != '1') || (x[1] != '0') || (x[2] != '1')) {
       client.stop();
       client = NULL;
+      //Serial.println("client stopped");
     }
   }
   return client;
